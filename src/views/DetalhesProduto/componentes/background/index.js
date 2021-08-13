ad4b9907abe04133b56b0e8a4ec70aed6a5a5565
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-08-13 16:02:20 -0300

    implementado a imagem background, a seta esquerda e a sacola de compras no componente detalhes do produto

--- src/views/DetalhesProduto/componentes/background/index.js
+++ src/views/DetalhesProduto/componentes/background/index.js
@@ -1,6 +1,56 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {View} from 'react-native';
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {Sacola} from '../../../../componentes/sacola';
 
 export const Background = () => {
-  return <View />;
+  const imgSrc = require('../../../../assets/images/bgimg.jpg');
+  const navigation = useNavigation();
+  return (
+    <View style={styles.bgContainer}>
+      <ImageBackground
+        resizeMode="cover"
+        source={imgSrc}
+        style={styles.imagemFundo}>
+        <View style={styles.headerContainer}>
+          <Image
+            source={require('../../../../assets/images/flecha-esquerda.png')}
+            style={styles.imagemSeta}
+          />
+          <View style={styles.containerSacola}>
+            <Sacola />
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  bgContainer: {
+    flex: 1.5,
+  },
+  imagemFundo: {
+    width: Dimensions.get('window').width,
+    height: '100%',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  imagemSeta: {
+    width: 24,
+    height: 24,
+    marginTop: 36,
+    marginLeft: 24,
+  },
+  containerSacola: {
+    padding: 18,
+  },
+});
